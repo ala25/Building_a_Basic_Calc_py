ca93b4b32791f6e34735d4146d753ea1ab87cb60
Andy Altamirano <andyalta@Andys-MacBook-Air.local>
2025-11-22 14:16:21 -0500

    adding the code and logic

--- calculator.py
+++ calculator.py
@@ -1,1 +1,6 @@
-# basic calculator tha takes two inputs from the user and addes them together
+# basic calculator tha takes two float number inputs from the user and adds them together
+
+num1  = float(input("Enter a number: "))
+num2 = float(input("Enter another number: "))
+
+print("The sum is: " + str(num1 + num2))
